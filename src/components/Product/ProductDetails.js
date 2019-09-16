2570389cb952113a3131e0faf385ccceeee90088
Praecor Loth <praecorloth@gmail.com>
2019-09-16 15:38:38 +0200

    fixed product details

--- src/components/Product/ProductDetails.js
+++ src/components/Product/ProductDetails.js
@@ -30,7 +30,7 @@ const ProductDetails = ({ product, categorySlug }) => {
   const { title, price, image, description, images, promo } = product;
 
   const [mainImage, setMainImage] = useState(image);
-  const [galleryImages] = useState([mainImage, ...images]);
+  const [galleryImages] = useState(images);
 
   // Changes main image to the one that is selected in the gallery
   const setAsMainImage = id => {
