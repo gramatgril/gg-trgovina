43eb8875e15b189e2eea5b888ac91ed14698f6f0
Praecor Loth <praecorloth@gmail.com>
2019-10-22 00:06:29 +0200

    back to google map

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -74,14 +74,14 @@ const Contact = ({ companyInfo, contactInfo }) => {
           <p>{companyDesc}</p>
         </div>
       </Container>
-      <LeafletMap />
-      {/* <Map
+      {/* <LeafletMap /> */}
+      <Map
         isMarkerShown
         googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyD7NYaR2FH2BDpiDmgUI9NaEBI4TDmgUOU&v=3.exp&libraries=geometry,drawing,places`}
         loadingElement={<div style={{ height: `100%` }} />}
         containerElement={<div style={{ height: `400px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
-      /> */}
+      />
     </Wrapper>
   );
 };
