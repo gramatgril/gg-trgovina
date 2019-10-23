aa13cb1a32ea46a88b2a7a274cdb3c0724d25dc5
Praecor Loth <praecorloth@gmail.com>
2019-10-23 12:06:13 +0200

    t

--- functions/test.js
+++ functions/test.js
@@ -12,40 +12,55 @@ const headers = {
 exports.handler = async (event, context) => {
   const { name, email, message } = JSON.parse(event.body);
 
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.GOOGLE_EMAIL_USER,
-      pass: process.env.GOOGLE_EMAIL_PASS,
-    },
-  });
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      headers,
+      body: JSON.stringify({ msg: "Method not allowed" }),
+    };
+  }
 
-  let mailOptions = {
-    from: email,
-    to: process.env.GOOGLE_EMAIL_USER,
-    subject: `Povpraševanje: ${name}`,
-    text: message,
-    html: `<h3>Besedilo:</h3>
+  if (!name || !email || !message) {
+    return {
+      statusCode: 400,
+      headers,
+      body: JSON.stringify({ msg: "Fields missing" }),
+    };
+  } else {
+    sendMail();
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify({ msg: "Success" }),
+    };
+  }
+
+  function sendMail() {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GOOGLE_EMAIL_USER,
+        pass: process.env.GOOGLE_EMAIL_PASS,
+      },
+    });
+
+    let mailOptions = {
+      from: email,
+      to: process.env.GOOGLE_EMAIL_USER,
+      subject: `Povpraševanje: ${name}`,
+      text: message,
+      html: `<h3>Besedilo:</h3>
               <p>${message}</p>
               <h3>Poslal:</h3>
               <p> ${email}</p>`,
-  };
+    };
 
-  transporter.sendMail(mailOptions, (err, data) => {
-    if (err) {
-      console.log("Email not sent");
-      return {
-        statusCode: 400,
-        headers,
-        body: JSON.stringify({ msg: "Fail" }),
-      };
-    } else {
-      console.log("Email sent");
-      return {
-        statusCode: 200,
-        headers,
-        body: JSON.stringify({ msg: "Success" }),
-      };
-    }
-  });
+    transporter.sendMail(mailOptions, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Email sent");
+      }
+    });
+  }
 };
