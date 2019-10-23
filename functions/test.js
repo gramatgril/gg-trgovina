9fe64adef1810827b75cf2aef09425eb54f49744
Praecor Loth <praecorloth@gmail.com>
2019-10-23 13:39:33 +0200

    working?

--- functions/test.js
+++ functions/test.js
@@ -11,34 +11,29 @@ const headers = {
 
 exports.handler = async (event, context) => {
   const { name, email, message } = JSON.parse(event.body);
-  console.log("message:", message);
-  console.log("email:", email);
-  console.log("name:", name);
 
-  // async function sendMail() {
-  //   let transporter = nodemailer.createTransport({
-  //     service: "gmail",
-  //     auth: {
-  //       user: process.env.GOOGLE_EMAIL_USER,
-  //       pass: process.env.GOOGLE_EMAIL_PASS,
-  //     },
-  //   });
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GOOGLE_EMAIL_USER,
+      pass: process.env.GOOGLE_EMAIL_PASS,
+    },
+  });
 
-  //   let info = await transporter.sendMail({
-  //     from: email,
-  //     to: process.env.GOOGLE_EMAIL_USER,
-  //     subject: `Povpraševanje: ${name}`,
-  //     text: message,
-  //     html: `<h3>Besedilo:</h3>
-  //             <p>${message}</p>
-  //             <h3>Poslal:</h3>
-  //             <p> ${email}</p>`,
-  //   });
-
-  //   console.log(`Sender: ${email}, MessageId: ${info.messageId}`);
-  // }
-
-  // sendMail();
+  transporter
+    .sendMail({
+      from: email,
+      to: process.env.GOOGLE_EMAIL_USER,
+      subject: `Povpraševanje: ${name}`,
+      text: message,
+      html: `<h3>Besedilo:</h3>
+              <p>${message}</p>
+              <h3>Poslal:</h3>
+              <p> ${email}</p>`,
+    })
+    .catch(error => {
+      console.log(error);
+    });
 
   return {
     statusCode: 200,
